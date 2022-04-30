836f09ba3738c8d0d00fc17a3e2dc0460f4b8e49
Victor Krylach <vkrya2020@gmail.com>
2022-04-30 15:50:42 +0300

    Odd smile is ready

--- lab3/draw_odd_smile.py
+++ lab3/draw_odd_smile.py
@@ -9,7 +9,18 @@ RED = (138, 3, 3)
 BLACK = (0, 0, 0)
 screen = pygame.display.set_mode((400, 400))
 
+rect(screen, (240, 240, 240), (0, 0, 400, 400))
 circle(screen, YELLOW, (200, 200), 125)
+circle(screen, RED, (255, 179), 20)
+circle(screen, RED, (146, 179), 25)
+circle(screen, BLACK, (255, 179), 9)
+circle(screen, BLACK, (146, 179), 10)
+
+polygon(screen, BLACK, [[175, 171], [183, 159],
+						[82, 86], [75, 96]])
+polygon(screen, BLACK, [[223, 173], [217, 164],
+						[319, 110], [325, 119]])
+rect(screen, BLACK, (155, 270, 100, 23))
 
 
 pygame.display.update()
